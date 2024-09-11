043b59fd6034195e4307e8add80f95384e1bc58b
RanditZX <daivaraditya36@gmail.com>
2024-09-11 13:10:43 +0700

    s

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\userController; 
+use App\Http\Controllers\userController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 Route::get('/{pathMatch}', function () {
