ac88f29d51393bf0505f3f02b20e857b08ddbf9a
daffalghi <152256525+daffalghi@users.noreply.github.com>
2024-09-28 16:31:38 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -38,6 +38,21 @@
         function hidePopup() {
             document.getElementById('popup').classList.add('hidden');
         }
+        function toggleSelection(id) {
+        const btn = document.getElementById(id);
+        if (btn.style.backgroundColor === 'rgb(249, 115, 22)') {  // Checking if the background is orange
+            // Unselecting - Revert to default styles
+            btn.style.backgroundColor = 'white';
+            btn.style.color = '#6B7280';  // Grey text
+            btn.style.border = '2px solid #F97316';  // Orange border
+        } else {
+            // Selecting - Apply selected styles
+            btn.style.backgroundColor = '#F97316';  // Orange background
+            btn.style.color = 'white';  // White text
+            btn.style.border = 'none';  // No border for selected
+        }
+    }
+
     </script>
     {{-- notification --}}
     <script>
