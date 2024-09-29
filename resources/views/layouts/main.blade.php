91994b01b210bf3bd0c08db4f353c4f2a3fbc687
daffalghi <daffaserangbanten@gmail.com>
2024-09-29 14:40:02 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,6 +35,7 @@
             },
         };
     </script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
 </head>
 
 <body>
