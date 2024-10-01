b69deff838bc07dbaf8844f5e5752d2a06720e27
Tresnaft <tresnaft.13@gmail.com>
2024-10-01 09:08:36 +0700

    update tambah beasiswa

--- database/migrations/2024_09_28_092021_create_beasiswa_table.php
+++ database/migrations/2024_09_28_092021_create_beasiswa_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->enum('jenis_waktu_beasiswa',['current','upcoming','last']);
             $table->enum('tipe_beasiswa',['ekonomi','prestasi','external']);
             $table->enum('jenis_beasiswa', ['full', 'setengah']); // enum jenis_beasiswa
-            $table->enum('tipe_beasiswa', ['prestasi', 'ekonomi', 'eksternal']); // enum jenis_beasiswa
             $table->integer('kuota');
             $table->string('sumber');
             $table->date('tanggal_mulai');
